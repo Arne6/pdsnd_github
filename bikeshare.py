0e5e6eb9f210475a4cc2c982d0e868efacf0251b
Arne List <arne.list@ece.com>
2022-03-14 08:52:29 +0100

    edited the comments in bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -16,7 +16,6 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     
     cities = ['chicago', 'new york city', 'washington'] 
     months = ['january', 'february', 'march', 'april', 'may', 'june', 'all']
@@ -141,7 +140,6 @@ def station_stats(df):
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
-
 
 def trip_duration_stats(df):
     """Displays statistics on the total and average trip duration."""
